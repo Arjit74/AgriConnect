66cf178bb121c4711ddc7b8d361dcbf099bb9e57
Arjit74 <sharmaarjit886@gmail.com>
2025-06-30 14:18:59 +0530

    Added Netlify.toml file

--- faar/vite.config.js
+++ faar/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/faar/' : '/',
   server: {
-    proxy:{
+    proxy: {
       '/api': "http://localhost:8000"
     }
   },
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
